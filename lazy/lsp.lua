5127b33e843653d0a1f9a8182127dfff06444c9a
Chandler Anderson <chandler.anderson@odaseva.com>
2024-05-28 11:27:41 -0400

    Add Apex settings

--- lazy/lsp.lua
+++ lazy/lsp.lua
@@ -56,6 +56,16 @@ return {
                         }
                     }
                 end,
+		["apex_ls"] = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.apex_ls.setup {
+				filetypes = { "cls", "trigger", "apex", "apexcode" },
+                		apex_jar_path = vim.fn.expand("$HOME/lib/apex-jorje-lsp.jar"),
+				apex_enable_semantic_errors = true, -- Whether to allow Apex Language Server to surface semantic errors
+				apex_enable_completion_statistics = false, -- Whether to allow Apex Language Server to collect telemetry on code completion usage
+			}
+		end,
+
             }
         })
 
