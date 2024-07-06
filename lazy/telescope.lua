8ad5b21d776e4b78ab41e39e7c8a3f9e78e9265a
Chandler Anderson <chandler.anderson@odaseva.com>
2024-07-05 22:08:15 -0400

    Add LSP ref shortcut

--- lazy/telescope.lua
+++ lazy/telescope.lua
@@ -1,32 +1,34 @@
 return {
-    "nvim-telescope/telescope.nvim",
+  "nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+  tag = "0.1.5",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-	"lrfurtado/telescope-gitlab.nvim",
-    },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "lrfurtado/telescope-gitlab.nvim",
+  },
 
-    config = function()
-        require('telescope').setup({})
-	require("telescope").load_extension("gitlab")
+  config = function()
+    require('telescope').setup({})
+    require("telescope").load_extension("gitlab")
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-	vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-    end
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
+    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>pws', function()
+      local word = vim.fn.expand("<cword>")
+      builtin.grep_string({ search = word })
+    end)
+    vim.keymap.set('n', '<leader>pWs', function()
+      local word = vim.fn.expand("<cWORD>")
+      builtin.grep_string({ search = word })
+    end)
+    vim.keymap.set('n', '<leader>ps', function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end)
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+    vim.keymap.set('n', '<leader>lr>', builtin.lsp_references, { noremap = true, silent = true })
+  end
 }
