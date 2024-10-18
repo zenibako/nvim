2e2190fac0baef1f8f73b33cd9b4a490646713a5
Chandler Anderson <18584424+zenibako@users.noreply.github.com>
2024-10-18 09:26:19 -0400

    Add tssorter

---
+++ lazy/tssorter.lua
@@ -0,0 +1,10 @@
+return {
+	"mtrajano/tssorter.nvim",
+	version = "*", -- latest stable version, use `main` to keep up with the latest changes
+	---@module "tssorter"
+	---@type TssorterOpts
+	opts = {
+		-- leave empty for the default config or define your own sortables in here. They will add, rather than
+		-- replace, the defaults for the given filetype
+	},
+}
