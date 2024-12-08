d9df65a346a7d6798d5a36dfe1bd3aa62b36d9fc
Chandler Anderson <18584424+zenibako@users.noreply.github.com>
2024-12-08 06:42:14 -0500

    Add lazy/mini

---
+++ lazy/mini.lua
@@ -0,0 +1,10 @@
+return {
+	"echasnovski/mini.nvim",
+	version = false,
+	setup = function()
+		require("mini.ai").setup()
+		require("mini.operators").setup()
+		require("mini.pairs").setup()
+		require("mini.surround").setup()
+	end,
+}
