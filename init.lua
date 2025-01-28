1ee7b16082a2628060d21b65d36e06fe17c831f4
Chandler Anderson <18584424+zenibako@users.noreply.github.com>
2025-01-28 11:43:19 -0500

    Add oil

--- init.lua
+++ init.lua
@@ -6,10 +6,12 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.cmd("set number relativenumber")
 
 -- Miscellaneous remaps. More remaps can be found in the individual plugin Lua files.
+local file_mgr_cmd = "Oil"
+
 vim.keymap.set("n", "<leader>glmr", "<cmd>!glab mr create --web --no-editor --fill<CR>")
 vim.keymap.set("n", "<leader>fr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd[file_mgr_cmd])
 
 require("zenibako.formatting")
 require("zenibako.lazy_init")
