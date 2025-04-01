fd308bdd0dc41d6550c3eea1eb72658dcb145e92
Chandler Anderson <18584424+zenibako@users.noreply.github.com>
2025-04-01 17:46:14 -0400

    fix: root markers for apex_ls

--- lsp/apex_ls.lua
+++ lsp/apex_ls.lua
@@ -1,5 +1,6 @@
 local config = {
-  filetypes = { "java", "trigger", "apex" },
+  filetypes = { "java", "trigger", "apex", "apexcode" },
+  root_markers = { "sfdx-project.json" },
   apex_jar_path = vim.fn.expand("$HOME/lib/apex-jorje-lsp.jar"),
   apex_enable_semantic_errors = true,      -- Whether to allow Apex Language Server to surface semantic errors
   apex_enable_completion_statistics = true, -- Whether to allow Apex Language Server to collect telemetry on code completion usage
