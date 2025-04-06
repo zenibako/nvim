e6a757f27ec59d4d911fa8401d852b3a910190bc
Chandler Anderson <chanderson90@gmail.com>
2025-04-06 16:30:01 -0400

    refactor: Move keymaps to separate file

--- init.lua
+++ init.lua
@@ -2,22 +2,14 @@ vim.g.mapleader = " "
 
 require("config.formatting")
 require("config.lazy")
+require("config.keymaps")
 -- require("config.rocks")
 require("config.salesforce")
 
 vim.opt.hlsearch = true
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.cmd("set number relativenumber")
 vim.cmd("set mouse=")
-
--- Miscellaneous remaps. More remaps can be found in the individual plugin Lua files.
-local file_mgr_cmd = "Oil"
-
-vim.keymap.set("n", "<leader>glmr", "<cmd>!glab mr create --web --no-editor --fill<CR>")
-vim.keymap.set("n", "<leader>fr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
-vim.keymap.set("n", "<leader>pv", vim.cmd[file_mgr_cmd])
 
 vim.api.nvim_create_autocmd("TermOpen", {
 	group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
