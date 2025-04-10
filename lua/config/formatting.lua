5d14722c21478be46eb0dde114a0322819845822
Chandler Anderson <18584424+zenibako@users.noreply.github.com>
2025-04-10 06:43:55 -0400

    feat: Add virtual lines

--- lua/config/formatting.lua
+++ lua/config/formatting.lua
@@ -3,3 +3,16 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set expandtab")
 
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.diagnostic.config({
+  -- update_in_insert = true,
+  float = {
+    focusable = false,
+    style = "minimal",
+    border = "rounded",
+    source = "always",
+    header = "",
+    prefix = "",
+  },
+  virtual_lines = true
+})
