6df5c82757d1efa449181ee3bddc4621fcad8f59
Chandler Anderson <18584424+zenibako@users.noreply.github.com>
2025-05-07 12:11:42 -0400

    feat: re-enable salesforce plugin

---
+++ lua/plugins/salesforce.lua
@@ -0,0 +1,29 @@
+return {
+	"zenibako/salesforce.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("salesforce").setup({
+			debug = {
+				to_file = true,
+				to_command_line = false,
+			},
+			popup = {
+				-- The width of the popup window.
+				width = 160,
+				-- The height of the popup window.
+				height = 45,
+				-- The border characters to use for the popup window
+				borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+			},
+			file_manager = {
+				ignore_conflicts = false, -- ignores conflicts on "sf project retrieve/deploy"
+			},
+			org_manager = {
+				default_org_indicator = "󰄬",
+			},
+		})
+	end,
+}
