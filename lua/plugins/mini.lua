107c1b1afdbca954fb1abb2ca77e48296cd66fa1
Chandler Anderson <18584424+zenibako@users.noreply.github.com>
2025-05-17 12:06:52 -0400

    feat: use previews in mini.files

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -5,13 +5,21 @@ return {
 		require("mini.ai").setup()
 		require("mini.files").setup({
       mappings = {
-        go_in_plus = "<CR>"
-      }
+        go_in_plus = "<CR>",
+      },
+      windows = {
+        preview = true,
+        width_focus = 50,
+        width_nofocus = 15,
+        width_preview = 50,
+      },
     })
 		require("mini.move").setup()
 		require("mini.operators").setup()
 		require("mini.pairs").setup()
 		require("mini.surround").setup()
+    require("mini.snippets").setup()
 		require("mini.tabline").setup()
+    require("mini.fuzzy").setup()
 	end,
 }
