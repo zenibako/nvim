d905344cecdb77b09d0648216e63b31ca29021f2
Chandler Anderson <18584424+zenibako@users.noreply.github.com>
2025-05-17 12:06:05 -0400

    feat: migrate to blink from cmp

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,17 +1,33 @@
 return {
-  "zbirenbaum/copilot-cmp",
-  event = "InsertEnter",
-  config = function()
-    require("copilot_cmp").setup()
-  end,
-  dependencies = {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    config = function()
-      require("copilot").setup({
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-      })
-    end,
-  },
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		opts = {
+			suggestion = { enabled = false },
+			panel = { enabled = false },
+			filetypes = {
+				markdown = true,
+				help = true,
+			},
+		},
+	},
+	{
+		"saghen/blink.cmp",
+		optional = true,
+		dependencies = { "fang2hou/blink-copilot" },
+		opts = {
+			sources = {
+				default = { "copilot" },
+				providers = {
+					copilot = {
+						name = "copilot",
+						module = "blink-copilot",
+						score_offset = 100,
+						async = true,
+					},
+				},
+			},
+		},
+	},
 }
