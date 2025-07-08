2b6a0670fda3af8ba529a69ef094476b755bc3ab
Chandler Anderson <chandler.anderson@odaseva.com>
2025-07-08 16:08:15 -0400

    chore: remove work config

--- lua/config/salesforce.lua
+++
@@ -1,4 +0,0 @@
--- Recognize Salesforce extensions so proper LSPs activate
-vim.filetype.add({
-	extension = { cls = "apex", trigger = "apex", page = "visualforce", component = "visualforce", apex = "apex" },
-})
